23d00fcb112a0c224f62dc110b93820392c0330b
sharathy27 <sharathkumaryuvaraj@gmail.com>
2025-05-11 21:25:23 +0530

    updated configuration

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
